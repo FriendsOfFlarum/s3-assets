83335e5a046dea7da5d1d182b1e6dc3ccdf10d86
IanM <ian@morland.me>
2024-11-21 20:03:26 +0000

    wip

--- src/Content/AdminPayload.php
+++ src/Content/AdminPayload.php
@@ -11,14 +11,19 @@
 
 namespace FoF\S3Assets\Content;
 
+use Flarum\Foundation\Config;
 use Flarum\Frontend\Document;
 use Illuminate\Support\Arr;
 
 class AdminPayload
 {
+    public function __construct(
+        protected Config $config
+    ) {
+    }
+    
     public function __invoke(Document $document)
     {
-        $config = resolve('config');
-        $document->payload['s3SetByEnv'] = Arr::get($config->offsetGet('filesystems'), 'disks.s3.set_by_environment');
+        $document->payload['s3SetByEnv'] = Arr::get($this->config->offsetGet('filesystems'), 'disks.s3.set_by_environment');
     }
 }
